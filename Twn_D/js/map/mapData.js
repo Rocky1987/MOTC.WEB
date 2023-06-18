df9dedf2365792a2b1017b30e74e81a225e27980
DESKTOP-CMKFK60\g2030 <decievemyself@gmail.com>
2023-06-18 22:56:03 +0800

    新增繪製開發

--- Twn_D/js/map/mapData.js
+++ Twn_D/js/map/mapData.js
@@ -1,5 +1,13 @@
-const { createApp } = Vue
+const { createApp } = Vue;
   
+const config = {
+  domainName:"http://localhost:52579/",
+  projectName : "MOTC",
+  url:{
+    AddDrawResults : "api/MOTC/AddDrawResults"
+  }
+};
+
 let addApp = createApp({
   data() {
     return {
@@ -28,7 +36,56 @@ let addApp = createApp({
       mapConfig.source.removeFeature(featureById);    
     },
     addDrawResults:function(){
-      alert("ok");
+      let isAdd = confirm("確認新增本次繪製結果??");
+
+      if(!isAdd)
+      {
+        return;
+      }
+
+      const addTitleTxt = document.getElementById("addTitleTxt").value;
+
+      if(addTitleTxt === ""){
+        alert("請輸入標題名稱!");
+        return;
+      }
+
+      if(this.drawSymbolArr.length === 0){
+        alert("至少需繪製一筆圖徵!!");
+        return;
+      }
+
+      let tempDataArr = [];
+
+      //for(let i = 0; i < this.drawSymbolArr )
+
+      const data = {
+        DrawRecord:{
+          Title:addTitleTxt,
+          CreateAcc : document.getElementById("user-id").innerHTML
+        },
+        DrawDetailRecordList : this.drawSymbolArr
+      }
+
+      axios.post(
+        config.domainName + config.url.AddDrawResults, 
+        //mapData.data.Api.TestUrl + "api/FACOA/GetEventsData",
+        data
+        ).then(function (response) {
+          console.log(response);
+          let results = response.data;
+          if(results.Status === 1){        
+          document.getElementById("closeHistoryListBox").click();
+          document.getElementById("addTitleTxt").value = "";
+          alert("儲存成功");
+          }else{
+            console.log(response.ErrorMessage);
+            alert("儲存失敗");
+          }
+        });
+      
+     
+        
     }
   }
-}).mount('#historyListBox')
+}).mount('#historyListBox');
